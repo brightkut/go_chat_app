617e1288ebe520d874da5dd88d7da6cb2c5fb59d
LonEr <dsorn2@gmail.com>
2025-06-19 04:20:15 +0700

    feat: init fiber app

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/", hello)
+
+	log.Fatal(app.Listen(":8088"))
+}
+
+func hello(c *fiber.Ctx) error {
+	return c.SendString("Hello Chat App")
+}
